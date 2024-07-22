b4cecac5ae00d95c49fcfe3bbfc405cbfc5dd84c
Robert Schütz <github@dotlambda.de>
2024-07-22 07:59:35 -0700

    mark test as asyncio

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,12 @@
 """Test component setup."""
+import pytest
+
 from homeassistant.setup import async_setup_component
 
 from custom_components.govee_lan.const import DOMAIN
 
 
+@pytest.mark.asyncio
 async def test_async_setup(hass):
     """Test the component gets setup."""
     assert await async_setup_component(hass, DOMAIN, {}) is True
